35b1be00dec2a948e0241ae746284c2ad54f3b37
thandomsane273 <2947397@students.wits.ac.za>
2026-07-03 00:38:16 +0200

    Time: 6 ms (22.83%), Space: 45.8 MB (72.02%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,26 +1,25 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String number=String.valueOf(x).trim();
-        String[] comp=new String[number.length()];
-        for(int i=0; i<number.length(); i++){
-            comp[i]=String.valueOf(number.charAt(i));
-        }
+        // String[] comp=new String[number.length()];
+        // for(int i=0; i<number.length(); i++){
+        //     comp[i]=String.valueOf(number.charAt(i));
+        // }
 
         
-        boolean same=true;
+        //boolean same=true;
         int start=0;
-        int end=comp.length-1;
+        int end=number.length()-1;
 
         while (end>start){
-            if(!comp[start].equals(comp[end])){
-                same=false;
-                break;
+            if(number.charAt(start) != number.charAt(end)){
+                return false;
             }
             else{
                 start++;
                 end--;
             }
         }
-        return same;
+        return true;
     }    
 }
